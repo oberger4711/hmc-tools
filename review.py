be41a10f7b934e799083635b528d7f18ec3049e5
oberger <ole.k.berger@t-online.de>
2021-01-17 17:59:43 +0100

    Fix colors, add del functionality

--- review.py
+++ review.py
@@ -35,6 +35,7 @@ class Clip:
         self.mov_file_size = mov_file_size
         self.file_base_name = get_file_name_base(self.mov_file_path, MOV_FILE_ENDING)
         self.played = False
+        self.marked_for_del = False
 
 def find_clips(mts_dir, mov_dir):
     mts_file_pathes = find_file_pathes(mts_dir, MTS_FILE_ENDING)
@@ -63,11 +64,20 @@ class Model:
         self.clips = clips
 
 class CursesViewController:
+    CP_MARK = 1
+    CP_BAR = 2
+    CP_PLAY = 6
+    CP_DEL = 4
+
     class Mode(abc.ABC):
-        def __init__(self, vc, name, cursor_line_attr=0):
+        def __init__(self, vc, name, cursor_line_attr=0, cursor_line_del_attr=None):
             self.vc = vc
             self.name = name
             self.cursor_line_attr = cursor_line_attr
+            if cursor_line_del_attr is not None:
+                self.cursor_line_del_attr = cursor_line_del_attr
+            else:
+                self.cursor_line_del_attr = curses.color_pair(CursesViewController.CP_DEL)
 
         @abc.abstractmethod
         def update(self):
@@ -79,7 +89,7 @@ class CursesViewController:
 
     class PlayMode(Mode):
         def __init__(self, vc, p):
-            super().__init__(vc, "PLAY", curses.color_pair(vc.CP_HIGHLIGHT))
+            super().__init__(vc, "PLAY", curses.color_pair(CursesViewController.CP_PLAY))
             self.p = p
 
         def update(self):
@@ -94,14 +104,29 @@ class CursesViewController:
 
     class NormalMode(Mode):
         def __init__(self, vc):
-            super().__init__(vc, "NORMAL", curses.color_pair(vc.CP_MARK))
+            super().__init__(vc, "NORMAL", curses.color_pair(CursesViewController.CP_MARK))
 
         def update(self):
             pass
 
         def handle_input(self, enter_pressed):
             in_buf = self.vc.in_buf
-            if in_buf == "j":
+            if in_buf.startswith(":"):
+                # Multi charactar command is being typed in.
+                if enter_pressed:
+                    if in_buf == ":w":
+                        self.vc.save()
+                    elif in_buf == ":wq":
+                        self.vc.save()
+                        self.vc.exit_no_save()
+                    elif in_buf == ":q":
+                        self.vc.exit_no_save()
+                    elif in_buf == ":q!" or in_buf == ":cq":
+                        self.vc.exit_no_save_forced()
+                    return True
+                else:
+                    return False # Command not yet complete.
+            elif in_buf == "j":
                 self.vc.move_cursor_line(1)
             elif in_buf == "k":
                 self.vc.move_cursor_line(-1)
@@ -116,19 +141,21 @@ class CursesViewController:
             elif in_buf == " ":
                 p = self.vc.play_at_cursor_line()
                 self.vc.switch_mode(CursesViewController.PlayMode(self.vc, p))
+            elif in_buf == "d":
+                self.vc.toggle_del_at_cursor_line()
 
             return True
 
     def __init__(self, scr, model):
         self.scr = scr
         self.model = model
+        self.exit = False
         self.in_buf = ""
         self.last_in = 0
+        self.msg = None
         self.cursor_line = 0
         self.top_v_line = 0
-        self.CP_MARK = 1
-        self.CP_BAR = 2
-        self.CP_HIGHLIGHT = 3
+        self.init_curses()
         self.mode = CursesViewController.NormalMode(self)
         self.reset()
 
@@ -158,32 +185,32 @@ class CursesViewController:
         curses.curs_set(0)
         self.scr.timeout(100)
         curses.use_default_colors()
-        curses.init_pair(self.CP_MARK, -1, 7) # -1, 7
-        curses.init_pair(self.CP_BAR, 7, 0) # 7, 0
-        curses.init_pair(self.CP_HIGHLIGHT, 7, 2) # 2, 7
+        curses.init_pair(CursesViewController.CP_MARK, 11, 15) # 12, 15
+        curses.init_pair(CursesViewController.CP_BAR, 12, 7) # -1, 7
+        curses.init_pair(CursesViewController.CP_PLAY, curses.COLOR_GREEN, curses.COLOR_WHITE) # 2, 7
+        curses.init_pair(CursesViewController.CP_DEL, curses.COLOR_RED, -1) # curses.COLOR_RED, -1
         #if self.rows < 4 or self.cols < 25: exit(-1) # Should not be necessary.
 
     def reset_editor(self):
+        self.cursor_line = max(0, min(len(self.model.clips) - 1, self.cursor_line))
+        self.top_v_line = max(0, min(len(self.model.clips) - 1, self.top_v_line))
         self.rows_editor = self.rows - 2
         self.pad_editor = curses.newpad(len(self.model.clips) + 1, self.cols)
         for i, _ in enumerate(self.model.clips):
             self.refresh_line(i)
+        self.refresh_editor()
 
     def reset(self):
-        self.init_curses()
         # Build windows and pads.
         self.scr.clear()
         self.scr.refresh()
         # Title bar
         self.win_title_bar = curses.newwin(1, self.cols + 1, 0, 0)
-        self.win_title_bar.bkgd(curses.color_pair(self.CP_MARK))
         self.refresh_title_bar()
         # Editor
         self.reset_editor()
-        self.refresh_editor()
         # Status bar
         self.win_status_bar = curses.newwin(1, self.cols + 1, self.rows - 1, 0)
-        self.win_status_bar.bkgd(curses.color_pair(self.CP_MARK))
         self.refresh_status_bar()
 
     def refresh_title_bar(self):
@@ -196,34 +223,52 @@ class CursesViewController:
         else:
             mov_dir_truncated = self.model.mov_dir
         # Left aligned
-        self.__s_addstr(self.win_title_bar, 0, 0, label, curses.A_REVERSE)
+        self.__s_addstr(self.win_title_bar, 0, 0, label, curses.color_pair(CursesViewController.CP_BAR) | curses.A_REVERSE)
         dirs_text = self.__trunc_text(" {} / {}".format(self.model.mts_dir, mov_dir_truncated), self.cols - len(label) - len(prog_name))
         self.__s_addstr(self.win_title_bar, 0, len(label), dirs_text)
         # Right aligned
-        self.__s_addstr(self.win_title_bar, 0, self.cols - len(prog_name), prog_name, curses.A_REVERSE)
+        self.__s_addstr(self.win_title_bar, 0, self.cols - len(prog_name), prog_name, curses.color_pair(CursesViewController.CP_BAR) | curses.A_REVERSE)
         self.win_title_bar.refresh()
 
     def refresh_status_bar(self):
         self.win_status_bar.clear()
         padded_mode_name = " {} ".format(self.mode.name)
         s_last_in = " {} ".format(self.last_in)
-        self.__s_addstr(self.win_status_bar, 0, 0, self.__trunc_text(padded_mode_name, self.cols - len(s_last_in)), curses.A_REVERSE | curses.A_BOLD)
-        self.__s_addstr(self.win_status_bar, 0, self.cols - len(s_last_in), s_last_in, curses.A_REVERSE)
+        self.__s_addstr(self.win_status_bar, 0, 0, self.__trunc_text(padded_mode_name, self.cols - len(s_last_in)), curses.color_pair(CursesViewController.CP_BAR) | curses.A_REVERSE | curses.A_BOLD)
+        if self.in_buf == "" and self.msg is not None:
+            self.__s_addstr(self.win_status_bar, 0, len(padded_mode_name), " {} ".format(self.msg), curses.color_pair(CursesViewController.CP_BAR))
+        else:
+            # Show current buffer.
+            self.__s_addstr(self.win_status_bar, 0, len(padded_mode_name), " {} ".format(self.in_buf), curses.color_pair(CursesViewController.CP_BAR))
+        self.__s_addstr(self.win_status_bar, 0, self.cols - len(s_last_in), s_last_in, curses.color_pair(CursesViewController.CP_BAR) | curses.A_REVERSE)
         self.win_status_bar.refresh()
 
     def refresh_line(self, i):
+        if i < 0 or i >= len(self.model.clips): return
         # Erase old line.
         self.pad_editor.move(i, 0)
         self.pad_editor.clrtoeol()
         clip = self.model.clips[i]
         s_file_size = " {:.1f} MB".format(clip.mov_file_size / (1024 * 1024))
-        s_file_name = self.__trunc_text(clip.file_base_name, self.cols - len(s_file_size))
-        attr = 0
+        stat = ""
         if not clip.played:
-            attr |= curses.A_BOLD
+            stat += "*"
+        if clip.marked_for_del:
+            stat += "D"
+        s_file_name = " {:2} {}".format(stat, clip.file_base_name)
+        s_file_name = self.__trunc_text(s_file_name, self.cols - len(s_file_size))
+        attr = 0
         if i == self.cursor_line:
+            if clip.marked_for_del:
+                attr |= self.mode.cursor_line_del_attr
+            else:
+                attr |= self.mode.cursor_line_attr
+            attr |= curses.A_REVERSE
             s_file_name = s_file_name + " " * (max(0, self.cols - len(s_file_name)))
-            attr |= self.mode.cursor_line_attr
+        else:
+            if clip.marked_for_del:
+                attr |= curses.color_pair(CursesViewController.CP_DEL)
+        if not clip.played: attr |= curses.A_BOLD
         self.__s_addstr(self.pad_editor, i, 0, s_file_name, attr)
         self.__s_addstr(self.pad_editor, i, self.cols - len(s_file_size), s_file_size, attr)
 
@@ -266,22 +311,53 @@ class CursesViewController:
         clip.played = True
         return subprocess.Popen(["cvlc", "--play-and-exit", mov_file_path], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
+    def toggle_del_at_cursor_line(self):
+        clip = self.model.clips[self.cursor_line]
+        clip.marked_for_del = not clip.marked_for_del
+        self.refresh_line(self.cursor_line)
+        self.refresh_editor()
+
     def switch_mode(self, mode):
         self.mode = mode
         self.refresh_status_bar()
         self.refresh_line(self.cursor_line)
         self.refresh_editor()
 
+    def exit_no_save_forced(self):
+        self.exit = True
+
+    def exit_no_save(self):
+        # Check if there are unsaved changes.
+        if any(c.marked_for_del for c in self.model.clips):
+            self.msg = "There are unsaved changes. Use :q! to force exit"
+            return
+        else:
+            self.exit = True
+
+    def save(self):
+        to_del = [c for c in self.model.clips if c.marked_for_del]
+        for c_to_del in to_del:
+            os.remove(c_to_del.mov_file_path)
+            os.remove(c_to_del.mts_file_path)
+            self.model.clips.remove(c_to_del)
+        if len(self.model.clips) == 0:
+            self.exit = True
+        self.reset()
+
     def loop(self):
-        while True:
+        while not self.exit:
             ch = self.scr.getch()
             if ch != -1:
+                self.msg = None # Reset error message.
                 if ch == curses.KEY_RESIZE:
                     # Terminal has been resized. Reset view.
+                    self.init_curses()
                     self.reset()
                 self.last_in = ch
                 if 0x20 <= self.last_in <= 0x7e:
                     self.in_buf += chr(self.last_in) # ASCII input goes to the buffer.
+                elif self.last_in == 263:
+                    self.in_buf = self.in_buf[:-1] # Backspace
                 enter_pressed = (self.last_in == 0xa)
                 if self.mode.handle_input(enter_pressed):
                     self.in_buf = "" # Reset buffer.
@@ -292,10 +368,6 @@ class CursesViewController:
 def show_curses_ui(scr, model):
     view_controller = CursesViewController(scr, model)
     view_controller.loop()
-
-def review_files(clips):
-    # TODO: Run cvlc --play-and-exit, which can be stopped with ctrl-w.
-    pass
 
 def main():
     args = parse_args()
@@ -319,6 +391,7 @@ def main():
     #for i in range(len(clips)):
     #    print(i)
     model = Model(mov_dir, mts_dir, clips)
+    # Start ncurses UI.
     curses.wrapper(show_curses_ui, model)
 
 if __name__ == "__main__":
